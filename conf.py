b62a519d420ae48953836bca39c94e48764b5eef
Marc Maxmeister <marcmaxmeister@gmail.com>
2019-06-05 21:11:04 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -61,14 +61,13 @@ locale_dirs = [
 gettext_compact = False
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = 'img/logo.svg'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
-
+#html_logo = 'img/logo.svg'
+#html_theme_options = {
+#    'logo_only': True,
+#    'display_version': False,
+#}
 
 epub_title = project
 epub_author = author
